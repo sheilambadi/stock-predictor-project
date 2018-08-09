6dc9ee749aa0b4b1afbf5857103087af2a9aff67
sheilambadi <mbadiatieno@gmail.com>
2018-08-10 00:12:06 +0300

    Add buttons and labels

--- reports.py
+++ reports.py
@@ -4,8 +4,12 @@ import tkinter.messagebox
 class DisplayReports():
 
     def __init__(self, master):
+        # Window 
+        self.master = master
+
         topFrame = Frame(master)
         topFrame.pack()
+        
         master.geometry('600x600')
         # menu at top
         menu = Menu(master)
@@ -22,11 +26,30 @@ class DisplayReports():
         helpMenu = Menu(menu)
         menu.add_cascade(label='Help', menu=helpMenu)
         helpMenu.add_command(label='About', command=self.about)
+
+        # middle Frame
+        self.middlePart()
+        # status bar
+        status = Label(root, text="Everything is ok :)", bd=1, relief=SUNKEN, anchor = W)
+        status.pack(side=BOTTOM, fill=X)
+        
   
     def about(self):
         tkinter.messagebox.showinfo('Stock Predictor', 'Version: 1.0.0\nDeveloper: Mbadi Atieno Sheila')
 
-        
+    def middlePart(self):
+        # company stock
+        '''
+        self.head = Label(self.master,text = 'Login',font = ('',35),pady = 10)
+        self.head.pack()
+        '''
+        self.middleFrame = Frame(self.master)
+       
+        Label(self.middleFrame,text = 'Stream Tweets: ',font = ('',20), pady=5,padx=5).grid(sticky = W)
+        Button(self.middleFrame,text = 'Get Tweets!      ' ,bd = 3 ,font = ('',15),padx=5,pady=5).grid(row=0,column=1, sticky=W)
+        Label(self.middleFrame,text = 'Stream Stock Data: ',font = ('',20), pady=5,padx=5).grid(sticky = W)
+        Button(self.middleFrame,text = 'Get Stock Data!' ,bd = 3 ,font = ('',15),padx=5,pady=5).grid(row=1,column=1, sticky=W)
+        self.middleFrame.pack()
 
 
 root = Tk()
