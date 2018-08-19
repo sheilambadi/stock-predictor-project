32d2dd12baa97c63406016561da2720ed676863b
sheilambadi <mbadiatieno@gmail.com>
2018-08-19 10:42:30 +0300

    Create file with appended dates for polarity

--- appended_file.py
+++ appended_file.py
@@ -13,6 +13,14 @@ def save_appended_file(ticker):
     new_df['Ticker'] = ticker.upper()
     new_df.to_csv(file_to_save, mode = 'a', header=False)
 
+    sum_file = "stock_polarity_sum_data.csv"
+    hybrid_columns = ['Date','Low','High','Close','Open','Volume', 'Adj Close', 'Polarity', 'Ticker']
+    # load training data
+    df = pd.read_csv('stock_polarity_data.csv', names = hybrid_columns)
+    sum_df = df.groupby(['Date', 'Ticker']).mean()
+    sum_df.to_csv(sum_file, mode = 'a', header=False)
+
+
 # list of stocks
 stockTickers = ['aapl', 'amzn', 'fb', 'gm', 'googl', 'msft', 'nflx', 'tsla', 'twtr']
 #stockTickers = ['aapl', 'amzn']
